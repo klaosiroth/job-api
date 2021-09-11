a0b4c830f0ff718af5f115c8232a0f03f811f088
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-12 02:46:31 +0700

    setup login - controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   // const { name, email, password } = req.body;
@@ -41,7 +41,23 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  // compare password
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  // generate token - instance method with mongoose
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
