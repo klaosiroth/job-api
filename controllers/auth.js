2885e9c00a9ed5cfff1b787ec0806e5b59e52052
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-12 00:59:29 +0700

    hash user password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
@@ -9,7 +10,16 @@ const register = async (req, res) => {
     throw new BadRequestError('Please provide name, email, and password');
   }
 
-  const user = await User.create({ ...req.body });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
