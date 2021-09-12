33ed49853e36e92133cea98b0b2a2c1610216f30
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-12 17:49:16 +0700

    added delete job route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -55,7 +55,21 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.NO_CONTENT).send();
 };
 
 module.exports = {
