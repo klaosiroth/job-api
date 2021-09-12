9686eac2984596358a4ad49ca834020bff175fc5
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-12 19:36:05 +0700

    security setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// express
 const express = require('express');
 const app = express();
 
@@ -10,18 +18,24 @@ const connectDB = require('./db/connect');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
-// error handler
+// middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticateUser = require('./middleware/authentication');
 
 app.use(express.json());
-// extra packages
+
+// security
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
+// error handler
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
